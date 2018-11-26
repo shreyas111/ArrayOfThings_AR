98ed117d20ea380c2c7c46de508c8f7a616d0a4c
shreyas111 <shreyaskulkarnilogin@gmail.com>
2018-11-25 23:40:34 -0600

    commit Latest

--- app/src/main/java/aot/cs491/com/aot_ar/LocationMarkerCustom.java
+++ app/src/main/java/aot/cs491/com/aot_ar/LocationMarkerCustom.java
@@ -10,6 +10,14 @@ import aot.cs491.com.aot_ar.aothttpapi.AOTNode;
 
 public class LocationMarkerCustom extends uk.co.appoly.arcorelocation.LocationMarker {
 
+    public AOTNode getAotNode() {
+        return aotNode;
+    }
+
+    public void setAotNode(AOTNode aotNode) {
+        this.aotNode = aotNode;
+    }
+
     private AOTNode aotNode;
     public LocationMarkerCustom (double longitude, double latitude, Node node, AOTNode aotNode) {
         super(longitude, latitude, node);
