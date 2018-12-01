70ab52a3e4dac9c76a619f1d63e4eadbd2c5081d
Mayank K Rastogi <mayankrastogi.is@gmail.com>
2018-11-30 19:20:47 -0600

    Add splash screen

--- app/src/main/java/aot/cs491/com/aot_ar/MainActivity.java
+++ app/src/main/java/aot/cs491/com/aot_ar/MainActivity.java
@@ -95,6 +95,8 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         arSceneView = findViewById(R.id.ar_scene_view);
